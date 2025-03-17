d7841680c5ed8e1a9bad8353ee60da18f4b10fbd
chrisdiazaguilar <chrisdiaz0641@gmail.com>
2025-03-17 17:40:25 -0400

    Reviewing functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -108,3 +108,26 @@ function fruitProcessor(apples, oranges) {
 
 console.log(fruitProcessor(2,3));
 */
+
+//Reviewing Functions
+
+const calcAge = function(birthYeah) {
+    return 2037 - birthYeah;
+}
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+    const age = calcAge(birthYeah);
+    const retirement = 65 - age;
+
+    if(retirement > 0){
+        return retirement;
+    } else {
+        return -1;
+        console.log(`${firstName} has already retired`)
+    }
+}
+
+// return '${firstName} retires in ${retirement} years';
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
